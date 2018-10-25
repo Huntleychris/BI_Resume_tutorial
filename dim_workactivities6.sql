69c9ed0a173bc8a563e96e532ffe7ff89afa84e2
Christopher <chuntley@blackshellconsulting.com>
2018-10-24 21:07:41 -0700

    adding .gitignore for xlsx

--- dim_workactivities6.sql
+++ dim_workactivities6.sql
@@ -1,7 +1,14 @@
 USE [PowerBIResumeDemo]
 GO
 
-/****** Object:  Table [dbo].[dim_workactivities]    Script Date: 2/1/2018 4:06:26 PM ******/
+ALTER TABLE [dbo].[dim_workactivities] DROP CONSTRAINT [FK__dim_worka__compa__4E88ABD4]
+GO
+
+/****** Object:  Table [dbo].[dim_workactivities]    Script Date: 10/24/2018 7:58:58 PM ******/
+DROP TABLE [dbo].[dim_workactivities]
+GO
+
+/****** Object:  Table [dbo].[dim_workactivities]    Script Date: 10/24/2018 7:58:58 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,12 +19,14 @@ CREATE TABLE [dbo].[dim_workactivities](
 	[workactivitiesid] [int] IDENTITY(1,1) NOT NULL,
 	[activitydescription] [varchar](max) NULL,
 	[companyid] [int] NULL,
+	[workbegin] Date null,
+	[workend] date null
+
 PRIMARY KEY CLUSTERED 
 (
 	[workactivitiesid] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
 GO
 
 ALTER TABLE [dbo].[dim_workactivities]  WITH CHECK ADD FOREIGN KEY([companyid])
