33b26d0e9455c52f240b5c7d62ab0341207dc09e
Christopher <chuntley@blackshellconsulting.com>
2018-10-24 19:37:15 -0700

    edits to sql scripts for dimensions

--- dim_company2.sql
+++ dim_company2.sql
@@ -1,24 +1,37 @@
 USE [PowerBIResumeDemo]
 GO
 
-/****** Object:  Table [dbo].[dim_company]    Script Date: 10/21/2018 5:22:07 PM ******/
+ALTER TABLE [dbo].[dim_company] DROP CONSTRAINT [FK_dim_company_zipcodes]
+GO
+
+/****** Object:  Table [dbo].[dim_company]    Script Date: 10/24/2018 7:36:04 PM ******/
+DROP TABLE [dbo].[dim_company]
+GO
+
+/****** Object:  Table [dbo].[dim_company]    Script Date: 10/24/2018 7:36:04 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-DROP TABLE IF EXISTS  dim_company
-
 CREATE TABLE [dbo].[dim_company](
 	[companyid] [int] IDENTITY(1,1) NOT NULL,
 	[companyname] [varchar](200) NULL,
+	[title] varchar(200) NULL,
 	[zip] [varchar](20) NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[companyid] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[dim_company]  WITH CHECK ADD  CONSTRAINT [FK_dim_company_zipcodes] FOREIGN KEY([zip])
+REFERENCES [dbo].[zipcodes] ([zip])
+GO
+
+ALTER TABLE [dbo].[dim_company] CHECK CONSTRAINT [FK_dim_company_zipcodes]
 GO
 
 
--- dim_skills4.sql
+++ dim_skills4.sql
@@ -1,17 +1,23 @@
 USE [PowerBIResumeDemo]
 GO
 
-/****** Object:  Table [dbo].[dim_skills]    Script Date: 2/1/2018 4:06:13 PM ******/
+/****** Object:  Table [dbo].[dim_skills]    Script Date: 10/24/2018 6:34:44 PM ******/
+DROP TABLE if exists [dbo].[dim_skills]
+
+
+/****** Object:  Table [dbo].[dim_skills]    Script Date: 10/24/2018 6:34:44 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-DROP TABLE IF EXISTS dim_skills
+
 CREATE TABLE [dbo].[dim_skills](
 	[workskillid] [int] IDENTITY(1,1) NOT NULL,
+	[companyid] [int] ,
 	[description] [varchar](1500) NULL,
-	[yearsexperience] [int] NULL,
+	[yearsexperience] Date NULL,
+	[CalculatedYearsOfExp]  AS (CONVERT([decimal](4,2),datediff(month,[yearsexperience],getdate()))/(12)),
 PRIMARY KEY CLUSTERED 
 (
 	[workskillid] ASC
@@ -19,5 +25,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 
 GO
+
+
 
 
